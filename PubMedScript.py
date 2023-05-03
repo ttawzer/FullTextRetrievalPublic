f2229794606b647a6d4e5adc1c2da035819e6202
Tiffany Tawzer <ttawzer@luc.edu>
2023-05-03 12:36:18 -0500

    Cleanup and existing sheet handling

--- PubMedScript.py
+++ PubMedScript.py
@@ -5,27 +5,30 @@ Created on Tue Apr 11 09:28:32 2023
 @author: ttawzer
 """
 
-import numpy
+# import numpy
 import pandas as pd
 # This assigns a variable to the package name for convenience
 import requests
 import json
 import time
-import webbrowser
+# import webbrowser
+# webbrowser is useful if you want to open the URLs instead of writing to Excel
 
-dataorig = pd.read_excel('C:/Users/ttawzer/Desktop/ScriptRetrievalList.xlsx', usecols='D,B,W')
+dataorig = pd.read_excel('N:\Python\ScriptRetrievalList.xlsx', usecols='D,B,W')
+# open the Excel file and use article name, DOI, and author columns
 dforig = pd.DataFrame(dataorig)
+# create a dataframe of imported data
 dforig['index'] = dforig.index
-# print()
+# assign an index to the data
 
-data = pd.read_excel('C:/Users/ttawzer/Desktop/ScriptRetrievalList.xlsx', usecols='D')
+data = pd.read_excel('N:\Python\ScriptRetrievalList.xlsx', usecols='D')
 # open Excel file and choose article name column
 df = pd.DataFrame(data)
 # turn the column into a dataframe
 df = df.replace(' ','+', regex=True)
 # change all spaces to +
 df2 = pd.DataFrame({})
-# set up an empty dataframe to hold the pmids
+# set up an empty dataframe to hold the pmids later
 for i in range(0, len(df)):
 # for every item, starting at index 0 and ending and the last cell, do the following   
     title = df.iloc[i, -1]
@@ -37,32 +40,36 @@ for i in range(0, len(df)):
     record = response.json()
     # get the data from the JSON file as a dictionary
     pmid = record.get('esearchresult',{}).get('idlist')
-    # get the pmid from the dictionary
+    # get the pmid list from the dictionary
     df1 = pd.DataFrame(pmid)
+    # add the pmid list to a dataframe
     if len(df1.index)>1:
         url = f'https://pubmed.ncbi.nlm.nih.gov/?term={title}'
+        # if there is more than one record, generate a URL for a pubmed search of the title
         df1 = pd.DataFrame({url})
-  #      webbrowser.open(url, new=1, autoraise=True)
-        # if there is more than one record, open a PubMed search of the title
+        # make a dataframe for the url
     elif df1.empty:
         url = f'https://pubmed.ncbi.nlm.nih.gov/?term={title}'
+        # if there are no records, generate a URL for a pubmed search of the title
         df1 = pd.DataFrame({url})
-   #     webbrowser.open(url, new=1, autoraise=True)
-        # if there are no records, open a PubMed search of the title
+         # make a dataframe with the url
     else:
         pmid2 = df1.iloc[0, -1]
         # get the pmid from the datatable
         url = f'https://tb2lc4tl2v.search.serialssolutions.com/?V=1.0&sid=Entrez:PubMed&id=pmid:{pmid2}'
         df1 = pd.DataFrame({url})
-   #     webbrowser.open(url, new=1, autoraise=True) 
-        # open a Serials Solutions search using the pmid
+        # create a Serials Solutions search url for the pmid and put it in a dataframe
     df2 = pd.concat([df2, df1])
+    # add the current record's dataframe to the full list
     time.sleep(.5)
-    # wait a second before moving to the next row
+    # wait a half second before moving to the next row
 df2 = df2.reset_index()
-df2 = df2.reindex(dforig.index)
+# reset the index for df2
+# df2 = df2.reindex(dforig.index)
+# change the index of df2 to match dforig MAY NOT NEED
 dfresult = pd.concat([dforig, df2], axis=1).reindex(dforig.index)
-
-with pd.ExcelWriter('C:/Users/ttawzer/Desktop/PythonTest.xlsx', mode='a') as writer:  
+# add the url column to the original data from dforig
+with pd.ExcelWriter('N:\Python\ScriptRetrievalList.xlsx', mode='a', if_sheet_exists='new') as writer:  
     dfresult.to_excel(writer, sheet_name='Sheet2')
+    # write the final dataframe to a new sheet in an Excel file
    
